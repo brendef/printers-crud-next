13b822387701f960092aeb31953587b5f459ccee
brendef <brendan.defaria@gmail.com>
2023-02-06 21:40:17 +0200

    fixed broken toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default () => {
     return ( 
       <label className="relative inline-flex items-center cursor-pointer">
         <input type="checkbox" value="" className="sr-only peer" disabled={!filterByActivityState} checked={showActiveStateOnly} onChange={handleStateToggle} />
-        <div className={`w-11 h-6 bg-${filterByActivityState ? 'danger' : 'gray' }-300 rounded-full peer peer-focus:ring-4 peer-focus:ring-success-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-${filterByActivityState ? 'success' : 'gray' }-600`}></div>
+        <div className={`w-11 h-6 bg-danger-600 rounded-full peer peer-focus:ring-4 peer-focus:ring-success-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-success-600`}></div>
       </label>
 
     )
